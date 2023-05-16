fc29c6b75a7ffe65c966cd53909b318c52ca8257
Poonam <poonam.trifed@gmail.com>
2023-05-16 14:49:37 +0530

    connect to mongodb

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
 const app = express()
 
+mongoose.connect('mongodb+srv://poonam:<password>@cluster0.m1pp8fi.mongodb.net/',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+
+
+
+
 app.listen((process.env.PORT || 3000), console.log("Server is Connected"))
