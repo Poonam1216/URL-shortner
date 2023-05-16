44a7f618ec7707b5472fc4a64c15f3461eaede32
Poonam <poonam.trifed@gmail.com>
2023-05-16 15:20:42 +0530

    add rest apis

--- script.js
+++ script.js
@@ -3,12 +3,35 @@ const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
 const app = express()
 
-mongoose.connect('mongodb+srv://poonam:<password>@cluster0.m1pp8fi.mongodb.net/',
+mongoose.connect('mongodb+srv://poonam:poonam@cluster0.m1pp8fi.mongodb.net/',
 {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
 
 
 
